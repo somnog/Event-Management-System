7f41e8b1727cd14dc634ba786ac4e97dc30a55ab
Ina Abdirizak <sahracabdirisaq937@gmail.com>
2026-09-21 13:16:54 +0100

    fixed folder structure

--- apps/gateway/src/event/event.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EventController } from './event.controller';
-import { RabbitmqModule } from 'src/rabbitmq/rabbitmq.module';
-
-@Module({
-  imports: [RabbitmqModule],
-  controllers: [EventController],
-})
-export class EventModule {}
